cfcfc37db4ab35191f0ef61073d29680a306d829
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 11:53:16 -0400

    introducing source specs

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Source, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'schema' do
+    context 'required database columns' do
+      it { expect(subject).to have_db_column(:context).of_type(:string).with_options(null: true) }
+      it { expect(subject).to have_db_column(:language).of_type(:string).with_options(null: true) }
+      it { expect(subject).to have_db_column(:text).of_type(:text).with_options(null: true) }
+    end
+
+    context 'required indexes on columns' do
+      it { expect(subject).to have_db_index(:context) }
+      it { expect(subject).to have_db_index(:language) }
+      it { expect(subject).to have_db_index(:text) }
+    end
+  end
+
+  context 'relationships' do
+    it { expect(subject).to have_many(:translations) }
+  end
 end
