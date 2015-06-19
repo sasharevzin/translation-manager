cd5598b4c59164826a822d6747c293d039706c57
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 11:47:53 -0400

    addin specs for db_column and relationships

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Translation, type: :model do
-  let(:source) { FactoryGirl.create(:source) }
-  let(:translation) do
-    t = FactoryGirl.create(:translation)
-    source.translations << t
-    t
+  context 'schema' do
+    it { expect(subject).to have_db_column(:text).of_type(:text).with_options(null: true) }
+    it { expect(subject).to have_db_column(:language).of_type(:string).with_options(null: true) }
+    it { expect(subject).to have_db_column(:source_id).of_type(:integer).with_options(null: true) }
   end
 
   context 'relationships' do
-    it { expect(translation).to belong_to(:source) }
+    it { expect(subject).to belong_to(:source) }
   end
 end
