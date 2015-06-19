eef5d8a3be3167ac85c16b2ef3a526c548e07fdb
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 11:36:51 -0400

    adding new association with source

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,7 +1,3 @@
 class Translation < ActiveRecord::Base
-  has_many :translations,
-           -> { where('id != parent_id') },
-           class_name: "Translation",
-           foreign_key: "parent_id"
-  belongs_to :parent_translation, class_name: "Translation"
+  belongs_to :source
 end
