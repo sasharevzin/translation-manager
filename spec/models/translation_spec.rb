7c39a7b206303f6a73d0b4103fbd9355cc153354
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 11:51:25 -0400

    adding specs for indexes

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Translation, type: :model do
   context 'schema' do
-    it { expect(subject).to have_db_column(:text).of_type(:text).with_options(null: true) }
-    it { expect(subject).to have_db_column(:language).of_type(:string).with_options(null: true) }
-    it { expect(subject).to have_db_column(:source_id).of_type(:integer).with_options(null: true) }
+    context 'required database columns' do
+      it { expect(subject).to have_db_column(:source_id).of_type(:integer).with_options(null: true) }
+      it { expect(subject).to have_db_column(:language).of_type(:string).with_options(null: true) }
+      it { expect(subject).to have_db_column(:text).of_type(:text).with_options(null: true) }
+    end
+
+    context 'required indexes on columns' do
+      it { expect(subject).to have_db_index(:language) }
+      it { expect(subject).to have_db_index(:text) }
+      it { expect(subject).to have_db_index(:source_id) }
+    end
   end
 
   context 'relationships' do
