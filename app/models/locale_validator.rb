991d30043a4bde03975ba23d51e095ad0fb7d199
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 14:24:53 -0400

    adding the custom validator for locale

---
+++ app/models/locale_validator.rb
@@ -0,0 +1,10 @@
+require "iso"
+require "yaml"
+
+class LocaleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value.blank? || ISO::Tag.new(value).valid?
+      record.errors[attribute] << (options[:message] || "is not valid ")
+    end
+  end
+end
