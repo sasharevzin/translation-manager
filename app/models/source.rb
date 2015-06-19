4047ae9642a7ebee2bfe8e45307820179db40f7a
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-19 17:40:13 -0400

    adding option to accept nested attributes

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,5 +1,6 @@
 class Source < ActiveRecord::Base
   has_many :translations
+  accepts_nested_attributes_for :translations
 
   validates :text, presence: true
   validates :language, presence: true, locale: true
