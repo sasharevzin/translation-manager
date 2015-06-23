e8675646298fb6fe20ed3207714218875bdfdca9
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-23 12:55:59 -0400

    edit page with spec

--- spec/views/sources/edit.html.erb_spec.rb
+++ spec/views/sources/edit.html.erb_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "translations/edit", type: :view do
-  before(:each) do
-    @translation = assign(:translation, Translation.create!())
-  end
+RSpec.describe "sources/edit", type: :view do
 
-  it "renders the edit translation form" do
+  let!(:source){Fabricate(:source)}
+
+  it "renders edit source form" do
+    @source = assign(:source, source)
     render
 
-    assert_select "form[action=?][method=?]", translation_path(@translation), "post" do
+    assert_select "form[action=?][method=?]", source_path(source), "post" do
+      assert_select "select#source_language[name=?]", "source[language]"
+      assert_select "textarea#source_text[name=?]", "source[text]"
+      assert_select "input#source_context[name=?]", "source[context]"
+      assert_select "select#source_translations_attributes_0_language[name=?]",
+                    "source[translations_attributes][0][language]"
+      assert_select "textarea#source_translations_attributes_0_text[name=?]",
+                    "source[translations_attributes][0][text]"
     end
   end
 end
