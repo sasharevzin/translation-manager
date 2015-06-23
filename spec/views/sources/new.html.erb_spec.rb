536b0acde1b186955fe4b31e9d215e197031b8c7
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-23 11:42:10 -0400

    new spec

---
+++ spec/views/sources/new.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "sources/new", type: :view do
+
+  it "renders new source form" do
+    @source = assign(:source, Source.new)
+    assign(:translation, @source.translations.build)
+    render
+    assert_select "form[action=?][method=?]", sources_path, "post" do
+      assert_select "input#source_language[name=?]", "source[language]"
+      assert_select "textarea#source_text[name=?]", "source[text]"
+      assert_select "input#source_context[name=?]", "source[context]"
+      assert_select "input#source_translations_attributes_0_language[name=?]",
+                    "source[translations_attributes][0][language]"
+      assert_select "textarea#source_translations_attributes_0_text[name=?]",
+                    "source[translations_attributes][0][text]"
+    end
+  end
+end
