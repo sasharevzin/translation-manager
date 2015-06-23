ca5373e7c97b69f1a75566540d83aebebac63a24
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-23 11:54:05 -0400

    changes on index page

--- spec/views/sources/index.html.erb_spec.rb
+++ spec/views/sources/index.html.erb_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 RSpec.describe "sources/index", type: :view do
   let(:sources){2.times.collect{Fabricate(:source)}}
 
-  before(:each) do
-    assign(:sources, sources)
-  end
-
   it "renders a list of sources" do
+    assign(:sources, sources)
     render
+
     sources.each do |source|
-      assert_select "tr>td", :text => source.id.to_s
-      assert_select "tr>td", :text => source.text.to_s
-      assert_select "tr>td", :text => source.language.to_s
+      expect(rendered).to have_selector("table tr td", text: source.id.to_s)
+      expect(rendered).to have_selector("table tr td", text: source.text.to_s)
+      expect(rendered).to have_selector("table tr td", text: source.language.to_s)
     end
   end
 end
