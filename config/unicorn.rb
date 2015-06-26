20ac167a8dd4ae9a0a46b813135b3abf27bb63a5
Saurabh Purnaye <spurnaye@gmail.com>
2015-06-26 15:58:32 -0400

    unicorn and deployment file changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,8 @@ timeout 30
 
 # Listen on a Unix data socket
 listen '/var/www/translations/current/tmp/sockets/unicorn.sock', :backlog => 2048
-
+stderr_path "/var/www/translations/current/log/unicorn.stderr.log"
+stdout_path "/var/www/translations/current/log/unicorn.stdout.lgo"
 
 ##
 # REE
@@ -47,38 +48,6 @@ before_fork do |server, worker|
   end
 end
 
-
 after_fork do |server, worker|
-  ##
-  # Unicorn master loads the app then forks off workers - because of the way
-  # Unix forking works, we need to make sure we aren't using any of the parent's
-  # sockets, e.g. db connection
-
   ActiveRecord::Base.establish_connection
-  # Redis and Memcached would go here but their connections are established
-  # on demand, so the master never opens a socket
-
-
-  ##
-  # Unicorn master is started as root, which is fine, but let's
-  # drop the workers to git:git
-
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'git', 'git'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if RAILS_ENV == 'development'
-      STDERR.puts "couldn't change user, oh well"
-    else
-      raise e
-    end
-  end
 end
