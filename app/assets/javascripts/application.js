ba37781c488bff32b78cd49765c2d92cb9620ab5
Saurabh Purnaye <spurnaye@gmail.com>
2015-07-01 14:14:51 -0400

    refactoring javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,26 +22,42 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-var ready = function(){
-  $('.languageSelect').selectunique();
-  $('#translationFields').on('click', '.addMore', function(){
+var checkMceEnabled = function(){
+    var mceEnabledFlag = false;
     $('#translationFields tr:last textarea.tinymce').each(function(input){
+      mceEnabledFlag = true;
       tinymce.execCommand("mceRemoveEditor", false, $(this).attr('id'));
     })
+   return mceEnabledFlag;
+}
+
+var addRow = function(){
     var text = $("#translationFields tr:last").html();
     var lastTranslationCount = $("#translationFields tr:last").data('translation-count');
     var newTranslation = text.replaceAll(lastTranslationCount,lastTranslationCount+1);
     $("#translationFields tbody").append('<tr>' + newTranslation + '</tr>');
     $('.languageSelect').selectunique('refresh');
     $("#translationFields tr:last").data('translation-count', lastTranslationCount+1);
-    $('#translationFields textarea').each(function(input){
-      tinymce.execCommand("mceAddEditor", false, $(this).attr('id'));
-    })
-  });
+}
 
+var removeRow = function(){
   $('#translationFields').on('click', '.remove', function(){
     $(this).parent().parent().remove();
   });
+}
+var ready = function(){
+  $('.languageSelect').selectunique();
+  $('#translationFields').on('click', '.addMore', function(){
+    mceEnabled = checkMceEnabled();
+    addRow();
+    $('#translationFields textarea').each(function(input){
+      if(mceEnabled)
+        {
+          tinymce.execCommand("mceAddEditor", false, $(this).attr('id'));
+        }
+    })
+  });
+  removeRow();
 };
 
 $(document).ready(ready);
