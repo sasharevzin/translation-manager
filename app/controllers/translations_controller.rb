a93d05c6e29a39ee6c66fd59fac5c00e914babe2
Stephen Haslett <Stephen.Haslett@rgenerator.com>
2015-07-21 12:57:51 -0400

    Adding render nothing to translations_controller

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -4,20 +4,12 @@ class TranslationsController < ApplicationController
 
   def destroy
     @translation.destroy
-    # TODO - Add a redirect URL here or remove it completely.
-    #redirect_to sources_url, notice: 'Source and translations destroyed successfully'
+    render :nothing => true
   end
 
   private
 
   def populate_source
     @translation = Translation.find(params[:id])
-  end
-
-  # TODO We can probably remove this.
-  def source_params
-    params.require(:source)
-      .permit(:language, :text, :context,
-              translations_attributes: [:language, :text, :context, :id, :source_id])
   end
 end
