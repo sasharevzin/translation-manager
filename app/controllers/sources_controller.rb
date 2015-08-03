e7ba684d4096cd9a5deca7c3c690b185f3e0d8eb
sshaw <skye.shaw@gmail.com>
2015-08-03 09:39:50 -0400

    When paginating it helps to actually call *paginate*

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -6,8 +6,7 @@ class SourcesController < ApplicationController
   def index
     @sources = Source.where('text LIKE ?', "%#{params[:text]}%")
     @sources = @sources.where(language: params[:language]) if params[:language].present?
-    @sources.paginate(page: params[:page], per_page: params[:per_page])
-    flash.now[:alert] = 'No results found with given search criteria' if @sources.empty?
+    @sources = @sources.paginate(page: params[:page], per_page: params[:per_page])
   end
 
   def new
