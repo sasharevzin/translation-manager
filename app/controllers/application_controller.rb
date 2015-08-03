2bacf95e4681e40e940d4b34684ff584d793cd5c
sshaw <skye.shaw@gmail.com>
2015-08-03 09:38:06 -0400

    Make use of Bootstrap in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
+require 'bh-flash'
+
 # application controller
 class ApplicationController < ActionController::Base
+  helper Bh::Flash
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from TranslationCache::CacheError do |ex|
+    logger.error(ex)
+    flash.now[:alert] = "Failed to save translation in the cache: #{ex}"
+    render action: action_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :sources
-  resources :translations, :only => 'destroy'  
+  resources :translations, only: 'destroy'
+
+  get 'search', to: 'sources#search'
 
   root to: 'sources#index'
 end
