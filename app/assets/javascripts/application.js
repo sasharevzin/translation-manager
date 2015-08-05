422bbf3e23cab37deef9793dacca697ed7ec6052
sshaw <skye.shaw@gmail.com>
2015-08-05 09:20:55 -0400

    Remove some unnecessary JS and fix WYSIWYG toggling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,91 +1,82 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require jquery.selectunique.js
 //= require tinymce
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
+//= require_tree .
 
-var checkMceEnabled = function(){
-    var mceEnabledFlag = false;
-    $('#translationFields tr:last textarea.tinymce').each(function(input){
-      mceEnabledFlag = true;
-      tinymce.execCommand("mceRemoveEditor", false, $(this).attr('id'));
-    })
-   return mceEnabledFlag;
-}
+var editorHandler = function(){
+  $('.editor .btn').click(function(e) {
+      e.preventDefault();
 
-var removeEditor = function(){
-  $('.main').on('click','.removeEditor', function (){
-    $('textarea').each(function(idx){
-       tinymce.execCommand("mceRemoveEditor", false, $(this).attr('id'));
-     });
-  });
-}
+      var $editor = $('.editor'), $textareas = $('textarea');
 
-var addEditor = function(){
-  $('body').on('click', '.enable-editor', function(){
-    $('textarea').each(function(idx){
-	  tinymce.execCommand("mceAddEditor", false, $(this).attr('id'));
-     });
+      if($editor.hasClass('enabled')) {
+	$editor.removeClass('enabled');
+	$editor.addClass('disabled');
+
+	$textareas.each(function() {
+	  tinymce.execCommand('mceRemoveEditor', false, $(this).attr('id'));
+	});
+      }
+      else {
+	$editor.removeClass('disabled');
+	$editor.addClass('enabled');
+
+	$textareas.each(function() {
+	  tinymce.execCommand('mceAddEditor', false, $(this).attr('id'));
+	});
+      }
   });
 }
 
 var createTranslationRow = function() {
-    return $(  $('#translation-row').text().replace('//<![CDATA[', '').replace('//]]>', '') );
+  return $(  $('#translation-row').text().replace('//<![CDATA[', '').replace('//]]>', '') );
 };
 
 var addRow = function() {
-    var newRow = createTranslationRow(), rowNumber = $('.translation').size() + 1;
+  var newRow = createTranslationRow(), rowNumber = $('.translation').size() + 1;
 
-    var selectBox = newRow.find('td select');
-    selectBox.attr('name', 'source[translations_attributes]['+ rowNumber +'][language]');
-    selectBox.attr('id', 'source_translations_attributes_'+ rowNumber +'_language');
+  var selectBox = newRow.find('td select');
+  selectBox.attr('name', 'source[translations_attributes]['+ rowNumber +'][language]');
+  selectBox.attr('id', 'source_translations_attributes_'+ rowNumber +'_language');
 
-    var textArea = newRow.find('td textarea');
-    textArea.attr('name', 'source[translations_attributes]['+ rowNumber +'][text]');
-    textArea.attr('id', 'source_translations_attributes_'+ rowNumber + '_text');
+  var textArea = newRow.find('td textarea');
+  textArea.attr('name', 'source[translations_attributes]['+ rowNumber +'][text]');
+  textArea.attr('id', 'source_translations_attributes_'+ rowNumber + '_text');
+
 
     $('#translationFields tbody').append(newRow);
 
-    $('.languageSelect').selectunique('refresh');
+    if($('.editor').hasClass('enabled')) {
+	var id = textArea.attr('id');
+	// Must be in the DOM to work
+	tinymce.execCommand('mceAddEditor', false, id);
+	tinymce.execCommand('mceFocus', false, id);
+    }
+
+  $('select[id$=_language]').selectunique('refresh');
 };
 
-var removeRow = function(){
+var rowHandler = function(){
   $('body').on('click', '.translation .remove', function(e) {
-    console.log('aaaaa');
-      e.preventDefault();
+    e.preventDefault();
+
     $(this).parents('.translation').remove();
-    $('.languageSelect').selectunique('refresh');
+    $('select[id$=_language]').selectunique('refresh');
   });
 }
+
 var ready = function(){
-  $('.languageSelect').selectunique();
+  $('select[id$=_language]').selectunique();
   $('body').on('click', '.add-translation', function(e) {
     e.preventDefault();
-    mceEnabled = checkMceEnabled();
     addRow();
   });
 
-  removeRow();
-  addEditor();
-  removeEditor();
+  editorHandler();
+  rowHandler();
 };
 
 $(document).ready(ready);
