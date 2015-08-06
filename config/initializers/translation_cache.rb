f4694e17a5be3f2bdc6bd9de64db609651f65f61
sshaw <skye.shaw@gmail.com>
2015-08-06 17:37:10 -0400

    Set Redis hostnames

--- config/initializers/translation_cache.rb
+++ config/initializers/translation_cache.rb
@@ -1,6 +1,15 @@
 I18n.enforce_available_locales = false
 
-redis = Redis.new
+host = case Rails.env
+       when 'staging'
+         'cache.labs.myplaydirect.com'
+       when 'production'
+         'cache.myplaydirect.com'
+       else
+         nil
+       end
+
+redis = Redis.new(host: host)
 begin
   redis.ping
   # Need to keep the original backend to support the default translations
